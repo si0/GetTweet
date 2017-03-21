dcb9e3f4bbe867bc0451747e9e99637c2e8a4ba9
si0 <si0@si0PC.local>
2017-03-22 01:42:22 +0900

    ツイート取得関係のアルゴリズム改修

--- gettweetpics.py
+++ gettweetpics.py
@@ -16,72 +16,45 @@ def getPics(user_id):
     # APIの作成
     api = tweepy.API(auth)
 
+    # ユーザーの存在確認
     try:
-        # 全ツイート数を取得
-        count = api.get_user(user_id).statuses_count - 1
-    except tweepy.error.TweepError:
-        print("ユーザーが存在しません。")
-        return "exists_user"
+        api.get_user(user_id)
+    except tweepy.error.TweepError as e:
+        if e.response.status_code == 404:
+            return "exists_user"
+    except:
+        return "error"
 
     # 保存先フォルダがない場合は作成
     if not os.path.isdir("./img"):
         os.mkdir("img")
-
     if not os.path.isdir("./img/" + user_id):
         os.mkdir("img/" + user_id)
 
-    tweet_count = 0 # ツイート数のカウント
-    tweet_page = 1 # ツイート毎のページ数のカウント
     file_number = 1 # ファイル名のカウント
 
-    # ツイートの取得
-    result = api.user_timeline(user_id, count=250, page=1)
-    result_len = len(result) - 1
+    for result in tweepy.Cursor(api.user_timeline, id=user_id).items():
+        # リツイートの場合は画像を取得しない
+        if hasattr(result, "retweeted_status"):
+            continue
+        # 画像付きツイートの判定
+        if "media" in result.entities:
+            # 画像の数(1ツイート内)の取得
+            media_len = len(result.extended_entities["media"])
 
-    try:
-        for i in range(count):
-            # 作業中のページが終了した場合は次のページへ
-            if tweet_count > result_len and result_len != 0:
-                tweet_count = 0
-                tweet_page = tweet_page + 1
-                result = api.user_timeline(user_id, count=result_len + 1, page=tweet_page)
-                result_len = len(result) - 1
-
-            # リツイートの場合は画像を取得しない
-            if hasattr(result[tweet_count], "retweeted_status"):
-                tweet_count = tweet_count + 1
-                continue
-
-            # 画像付きツイートの判断
-            elif "media" in result[tweet_count].entities:
-                # 画像の数(1ツイート内)の取得
-                media_len = len(result[tweet_count].extended_entities["media"])
-
-                # ツイート内の画像を保存
-                for n in range(media_len):
-                    # 画像URLの取得
-                    media_url = result[tweet_count].extended_entities["media"][n]["media_url"]
-                    # 拡張子の取得
-                    extension = ".%s" % media_url.split("/")[-1].split(".")[-1]
-                    # 元サイズの画像URLへURLの書き換え
-                    media_url = "%s:orig" % media_url
-                    # ファイル名の設定
-                    file_name = user_id + "_" + str(file_number) + extension
-                    file_number = file_number + 1
-                    # 画像の保存
-                    download(media_url, user_id, file_name)
-
-                tweet_count = tweet_count + 1
-
-            else:
-                tweet_count = tweet_count + 1
-
-    except IndexError as e:
-        print("エラー: " + str(e))
-        print("ツイートページ: " + str(tweet_page))
-        print("ツイートカウント: " + str(tweet_count))
-        print("ツイート総数: " + str(count))
-        return "error"
+            # ツイート内の画像を保存
+            for n in range(media_len):
+                # 画像URLの取得
+                media_url = result.extended_entities["media"][n]["media_url"]
+                # 拡張子の取得
+                extension = ".%s" % media_url.split("/")[-1].split(".")[-1]
+                # 元サイズの画像URLへ切り替え
+                media_url = "%s:orig" % media_url
+                # ファイル名の設定
+                file_name = user_id + "_" + str(file_number) + extension
+                file_number += 1
+                # 画像の保存
+                download(media_url, user_id, file_name)
 
 
 def download(media_url, user_id, file_name):
