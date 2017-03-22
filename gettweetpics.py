14f608a7879c1a94829ac99d4353d0a6f2307177
si0 <si0@si0PC.local>
2017-03-22 16:20:19 +0900

    ツイート検索機能の追加

--- gettweetpics.py
+++ gettweetpics.py
@@ -57,6 +57,47 @@ def getPics(user_id):
                 download(media_url, user_id, file_name)
 
 
+def getTweet(user_id, keyword_arg):
+    # twitterdevelopertool認証
+    CONSUMER_KEY = config.authkey["CON_KEY"]
+    CONSUMER_SECRET = config.authkey["CON_SEC"]
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    ACCESS_TOKEN = config.authkey["ACC_TOK"]
+    ACCESS_SECRET = config.authkey["ACC_SEC"]
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+    # apiの作成
+    api = tweepy.API(auth)
+
+    # 検索に一致したツイート内容のリスト
+    result_list = []
+
+    # ツイート情報の取得
+    result = tweepy.Cursor(api.user_timeline, id=user_id).items()
+
+    # 大文字小文字のどちらも検索可能にする
+    keyword = re.compile(keyword_arg, re.IGNORECASE)
+
+    for row in result:
+        # キーワードのツイートを判定する
+        if re.search(keyword, row.text):
+            # リストに追加
+            result_list.append({
+                "text": row.text,
+                "created_at": row.created_at
+                })
+
+    print("######################################")
+    print("検索が完了しました")
+    print("######################################\n")
+
+    # 取得した検索結果の表示
+    for row in result_list:
+        print("ツイート    : %s" % row["text"])
+        print("日時(UTC+0) : %s" % row["created_at"])
+        print("---------------------------------------\n")
+
+
 def download(media_url, user_id, file_name):
     # URL先ステータスの取得
     res = requests.get(media_url)
